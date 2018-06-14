a4c33c8383313dac5ed9af108634be545f242342
Túlio Parreiras <tulio.parreiras@hotmail.com>
2018-06-13 21:59:08 -0300

    Teste
    Metodo sorteia Dado

--- src/jogo/Teste.java
+++ src/jogo/Teste.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test;
 
 class Teste {
 
+	
+	/**Testando retorno do metodo SorteiaDado*/
 	@Test
 	void test() {
 		SorteiaDado teste = new SorteiaDado();
 		assertTrue(teste.getDado1() >0 && teste.getDado1() <7);
 	}
-
+	void test1() {
+		SorteiaDado teste = new SorteiaDado();
+		assertTrue(teste.getDado2() >0 && teste.getDado2() <7);
+	}
+	void test2() {
+		SorteiaDado teste = new SorteiaDado();
+		assertTrue(teste.getDado3() >0 && teste.getDado3() <7);
+	}
 	}
 
